27149f2c6a5bc0ecbd1714df7518c6f1f7db6ce5
TanmoyBD <tanmoymondal1315@gmail.com>
2023-10-06 17:03:37 +0600

    Update product details and settings

--- HEXASHOP/settings.py
+++ HEXASHOP/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'HEXASHOP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
